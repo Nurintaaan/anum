853b6b2b904daa531f12dcf77cd7680dd9922201
novialia <novialia12@gmail.com>
2018-05-08 21:19:12 +0700

    fix soal 2 udah

--- Tugas3/Soal_2/mainpoblano.m
+++ Tugas3/Soal_2/mainpoblano.m
@@ -1,4 +1,8 @@
 function mainpoblano()
+  warning off;
+  diary('result_soal2.txt');
+  diary on;
+  
   fprintf('------ FUNCTION A ------\n');
   poblanoFunction(@AFunction, [1 2]');
 
@@ -6,19 +10,23 @@ function mainpoblano()
   poblanoFunction(@BFunction, [1 1 -0.5]');
   
   fprintf('------ FUNCTION C ------\n');
-  x = [-400 -200 200 400];
+  x = [-400 -200 200 400 -400 -200 200 400];
   poblanoFunction(@GRFunction, [x]');
+  poblanoFunction(@GRFunction, [x x]');
+  poblanoFunction(@GRFunction, [x x x]');
+  poblanoFunction(@GRFunction, [x x x x]');
+  poblanoFunction(@GRFunction, [x x x x x]');
 end;
 
 function poblanoFunction(fx, x)
   for tol  = [10e-4 10e-6 10e-8 10e-10 10e-12];
-    fprintf('------------ Limited Memory BFGS ------------\n');
+    fprintf('------------ Limited Memory BFGS tol = %d ------------\n', tol);
     tic;
-    res = lbfgs(fx, x);
+    res = lbfgs(fx, x, 'Display', 'final');
     toc;
-    fprintf('------------ Nonlinear Conjugate Gradient ------------\n');
+    fprintf('------------ Nonlinear Conjugate Gradient tol = %d ------------\n', tol);
     tic;
-    res = ncg(fx, x);
+    res = ncg(fx, x, 'Display', 'final');
     toc;
   end;
 end;
