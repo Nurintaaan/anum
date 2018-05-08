f2910d65c7aea4f32f01bf2f26765e055a11a2d8
JessiJ <jessicana69@gmail.com>
2018-05-08 23:08:43 +0700

    addAll

--- Tugas3/Quasi_Newton/quasiNewton.m
+++ Tugas3/Quasi_Newton/quasiNewton.m
@@ -2,7 +2,7 @@
 % https://github.com/yyc9268/Numerical_optimization/blob/49639aa66b0f1dd47803110ae47b1083e4217b51/matlab/multivariate_smooth/quasi_newton.m
 % modified sesuai kebutuhan soal
 
-function minX = quasiNewton(f, initial, tol, max_iter)
+function [minX ,iter] = quasiNewton(f, initial, tol, max_iter)
   x = initial;
   k = 0;
   n = length(initial);
@@ -16,12 +16,13 @@ function minX = quasiNewton(f, initial, tol, max_iter)
     end;
     
     p = -B * g;
-    a = armijoSearch(f, x, 1, p);
+    a = directLineSearch(f, x, 1, p, max_iter);
     x_old = x;
     x = x + a*p;    
     g_new = grad(f, x);
     B = bfgs(B, x - x_old, g_new - g);
     k++;
   endwhile;
-  minX = x
+  minX = x;
+  iter = k;
 end;
