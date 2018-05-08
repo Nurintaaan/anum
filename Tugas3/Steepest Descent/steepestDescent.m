e63b3e60fec20c0b714f25b9ffe58b9ca3cf19b0
Nur Intan Alatas <intannnalatas@gmail.com>
2018-05-08 12:57:17 +0700

    add some function

--- Tugas3/Steepest Descent/steepestDescent.m
+++ Tugas3/Steepest Descent/steepestDescent.m
@@ -1,11 +1,15 @@
 function [minX] =  steepestDescent(f, grad, initial, tol)
-  x = initial
-  k = 0
-  while(norm(grad(x), 2) > tol )
-    p = -grad(x)
-    a = armijoSearch(f, grad, x, 1, p)    
-    x = x + a*p;
-    k = k + 1;
+  x = initial;
+  k = 0;
+  MAX_STEP = 1000;
+  while(norm(grad(x, f), 2) > tol)
+    % if(k == MAX_STEP) break; end;
+    p = -grad(x, f);
+    %a = armijoSearch(f, grad, x, 1, p)
+    a = armijoSearchV2(f, grad, x, 1, p)
+    %a = armijo(    1, x, p, f, grad)
+    x = x + a*p
+    k = k + 1
   end;
   x, k
 end;
